2220dcda3d34c63ce17f8c97943df64bedd31bbc
jessica-nam <jessicanam@nevada.unr.edu>
2023-12-05 09:41:48 -0800

    Added example dataset of Morgan's content Upload data to OpenAI and start a finetune job

--- Model/finetuning.py
+++ Model/finetuning.py
@@ -6,14 +6,14 @@ openai.api_key = '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'
 
 client = OpenAI()
 
-# # Training data needs to be uploaded using the Files API in order to be used with fine-tuning jobs
+# Training data needs to be uploaded using the Files API in order to be used with fine-tuning jobs
 # client.files.create(
 #     file=open("data.jsonl", "rb"),
 #     purpose="fine-tune"
 # )
 
-# Using the OpenAI SDK, start a fine-tuning job
+# Using the OpenAI SDK and the training file ID from above, start a fine-tuning job
 client.fine_tuning.jobs.create(
-    training_file="file-hROdaBZCBLy31KnD3Fq3wSwA", # Can find in OpenAI API interface
-    model="gpt-3.5-turbo" # char/completeion format
+    training_file="file-ctReRu28iP8pAJdFZstb0Yk9", # Can find in OpenAI API interface
+    model="gpt-3.5-turbo" # chat/completeion format
 )
