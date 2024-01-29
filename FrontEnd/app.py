0de1570a3e659fd79314fcb4a0030d01b5e5da62
jessica-nam <jessicanam@nevada.unr.edu>
2024-01-28 17:13:05 -0800

    Hooked up model to webpage

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import openai
+from openai import OpenAI
+
+client = OpenAI()
 
 app = Flask(__name__)
 
@@ -13,6 +17,17 @@ def ai_training():
 @app.route('/styling-and-format')
 def styling_and_format():
     return render_template('StylingAndFormat.html')
+
+@app.route('/generate-content', methods=['POST'])
+def generate_content():
+    user_prompt = request.form['user_prompt']
+    # Call AI model with the user_prompt
+    response = client.chat.completions.create(
+        model="ft:gpt-3.5-turbo-0613:personal::8Slov9kq", 
+        messages=[{"role": "user", "content": user_prompt}]
+    )
+    message_content = response.choices[0].message.content.strip()
+    return render_template('ContentGeneration.html', ai_response=message_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
