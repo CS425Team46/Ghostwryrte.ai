b4704c87e4432319574e8d3d5f8e1643fb6a8303
Africk-Hunter <hunterafrick@gmail.com>
2024-02-29 17:58:24 -0800

    Page loading errors fix and Log out functionality added
    There were errors when loading some content on the pages. The fix to it was to check if one of the elements from that page was loaded before allowing the functions to even be accessed. Additionally, I added functionality for a user to log out of their account.

--- FrontEnd/static/uploadFiles.mjs
+++ FrontEnd/static/uploadFiles.mjs
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDW-idC8yzOs7HlFZ21sB5-H7UjRZ6N6hs",
@@ -17,135 +17,153 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const fileUploadWindow = document.querySelector('.fileUploadWindow');
-    const uploadedDocsList = document.querySelector('.uploadedDocs');
+const fileUploadWindow = document.querySelector('.fileUploadWindow');
+const uploadedDocsList = document.querySelector('.uploadedDocs');
+const signUpForm = document.getElementById('signupForm');
+const LOButton = document.getElementById('LOButton');
 
-    if (fileUploadWindow) {
-        fileUploadWindow.addEventListener('dragover', (event) => {
-            event.preventDefault();
-            fileUploadWindow.classList.add('dragover');
-        });
+if (fileUploadWindow) {
+    fileUploadWindow.addEventListener('dragover', (event) => {
+        event.preventDefault();
+        fileUploadWindow.classList.add('dragover');
+    });
 
-        fileUploadWindow.addEventListener('dragleave', () => {
-            fileUploadWindow.classList.remove('dragover');
-        });
-    
-        fileUploadWindow.addEventListener('drop', (event) => {
-            event.preventDefault();
-            fileUploadWindow.classList.remove('dragover');
-    
-            const files = event.dataTransfer.files;
+    fileUploadWindow.addEventListener('dragleave', () => {
+        fileUploadWindow.classList.remove('dragover');
+    });
+
+    fileUploadWindow.addEventListener('drop', (event) => {
+        event.preventDefault();
+        fileUploadWindow.classList.remove('dragover');
+
+        const files = event.dataTransfer.files;
+        fileProcessing(files);
+    });
+
+    fileUploadWindow.addEventListener('click', () => {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = '.txt';
+        fileInput.multiple = true;
+
+        fileInput.click();
+
+        fileInput.addEventListener('change', (event) => {
+            const files = event.target.files;
             fileProcessing(files);
         });
+    });
 
-        fileUploadWindow.addEventListener('click', () => {
-            const fileInput = document.createElement('input');
-            fileInput.type = 'file';
-            fileInput.accept = '.txt';
-            fileInput.multiple = true;
-    
-            fileInput.click();
-    
-            fileInput.addEventListener('change', (event) => {
-                const files = event.target.files;
-                fileProcessing(files);
-            });
-        });
-    
 
-        // Add other event listeners related to fileUploadWindow here...
-    }
+    // Add other event listeners related to fileUploadWindow here...
+}
 
-    document.getElementById('signupForm').addEventListener('submit', function(event) {
+if(signUpForm){
+
+    signUpForm.addEventListener('submit', function(event) {
         event.preventDefault();
         const email = document.getElementById('signupEmail').value;
         const password = document.getElementById('signupPassword').value;
         handleSignUp(email, password);
-    });
+    }, false);
     
     document.getElementById('signinForm').addEventListener('submit', function(event) {
         event.preventDefault();
         const email = document.getElementById('signinEmail').value;
         const password = document.getElementById('signinPassword').value;
         handleSignIn(email, password);
-    });
+    }, false);
 
-    function fileProcessing(files) {
-        for (const file of files) {
-            if (file.type === 'text/plain') {
-                readAndUploadFile(file);
-            } else {
-                alert('Only .txt files are accepted.');
-            }
-        }
-    }
-    
-    function readAndUploadFile(file) {
-        const reader = new FileReader();
-    
-        reader.onload = function (e) {
-            const fileContent = e.target.result;
-            const title = extractTitle(fileContent);
-            fileUpload(title, fileContent);
-        };
-    
-        reader.readAsText(file);
-    }
-    
-    function extractTitle(fileContent) {
-        const sentences = fileContent.split('.');
-        return sentences[0].trim();
-    }
+}
 
-    function fileUpload(title, fileContent) {
-        const listItem = document.createElement('li');
-        listItem.textContent = title;
-        uploadedDocsList.appendChild(listItem);
+if(LOButton){
+    LOButton.addEventListener('click', function() {
+        console.log("Hello world");
+        signOut(auth).then(() => {
+            console.log('User signed out.');
+            window.location.href = '/'
+        }).catch((error) => {
+            console.error('Signout Error', error.code, error.message);
+        });
+    }, false);
+}
 
-        uploadToFirebase(title, fileContent);
-    }
 
-    async function uploadToFirebase(title, fileContent) {
-
-        const app = initializeApp(firebaseConfig); 
-        const db = getFirestore(app);
-
-        const user = auth.currentUser;
-        if (user) {
-            await setDoc(doc(db, 'users', user.uid, 'files', title), {
-                Title: title,
-                Content: fileContent,
-              });
+function fileProcessing(files) {
+    for (const file of files) {
+        if (file.type === 'text/plain') {
+            readAndUploadFile(file);
         } else {
-            console.error('No user is signed in to upload data');
+            alert('Only .txt files are accepted.');
         }
     }
-    
-    function handleSignUp() {
-        const email = document.getElementById('signupEmail').value;
-        const password = document.getElementById('signupPassword').value;
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(userCredential => {
-                console.log('Signup successful', userCredential.user);
-            })
-            .catch(error => {
-                console.error('Signup failed', error.code, error.message);
+}
+
+function readAndUploadFile(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const fileContent = e.target.result;
+        const title = extractTitle(fileContent);
+        fileUpload(title, fileContent);
+    };
+
+    reader.readAsText(file);
+}
+
+function extractTitle(fileContent) {
+    const sentences = fileContent.split('.');
+    return sentences[0].trim();
+}
+
+function fileUpload(title, fileContent) {
+    const listItem = document.createElement('li');
+    listItem.textContent = title;
+    uploadedDocsList.appendChild(listItem);
+
+    uploadToFirebase(title, fileContent);
+}
+
+async function uploadToFirebase(title, fileContent) {
+
+    const app = initializeApp(firebaseConfig); 
+    const db = getFirestore(app);
+
+    const user = auth.currentUser;
+    if (user) {
+        await setDoc(doc(db, 'users', user.uid, 'files', title), {
+            Title: title,
+            Content: fileContent,
             });
+    } else {
+        console.error('No user is signed in to upload data');
     }
-    
-    function handleSignIn() {
-        const email = document.getElementById('signinEmail').value;
-        const password = document.getElementById('signinPassword').value;
-        signInWithEmailAndPassword(auth, email, password)
-            .then(userCredential => {
-                console.log('Signin successful', userCredential.user);
-                window.location.href = '/content-generation'; // Redirect the user
-            })
-            .catch(error => {
-                console.error('Signin failed', error.code, error.message);
-            });
-    }
-});
+}
+
+function handleSignUp() {
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+            console.log('Signup successful', userCredential.user);
+        })
+        .catch(error => {
+            console.error('Signup failed', error.code, error.message);
+        });
+}
+
+function handleSignIn() {
+
+    const email = document.getElementById('signinEmail').value;
+    const password = document.getElementById('signinPassword').value;
+    signInWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+            console.log('Signin successful', userCredential.user);
+            window.location.href = '/content-generation'; // Redirect the user
+        })
+        .catch(error => {
+            console.error('Signin failed', error.code, error.message);
+        });
+}
 
 
